4d5be8182d6e27bc02771c3fc9ba1113049b5b62
Usuário Convidado <Guest@pa06micro24.local>
2016-08-15 20:35:17 -0300

    Initial Commit

---
+++ euSou/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  euSou
+//
+//  Created by Usuário Convidado on 15/08/16.
+//  Copyright © 2016 Sergio Magalhaes. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
